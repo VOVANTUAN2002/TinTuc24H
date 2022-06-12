3183191f03f56bcea1c6fe57df22f343b58416cc
An <chieman2k3@gmail.com>
2022-06-12 21:58:10 +0700

    view website

---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,248 @@
+@extends('frontend.layouts.master')
+@section('content')
+<!-- Top News Start-->
+<div class="top-news">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 tn-left">
+                <div class="tn-img">
+                    <img src="img/top-news-1.jpg" />
+                    <div class="tn-content">
+                        <div class="tn-content-inner">
+                            <a class="tn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                            <a class="tn-title" href="">Lorem ipsum dolor sit amet adipiscing elit</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 tn-right">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="tn-img">
+                            <img src="img/top-news-2.jpg" />
+                            <div class="tn-content">
+                                <div class="tn-content-inner">
+                                    <a class="tn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="tn-title" href="">Integer faucibus pharetra odio</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="tn-img">
+                            <img src="img/top-news-3.jpg" />
+                            <div class="tn-content">
+                                <div class="tn-content-inner">
+                                    <a class="tn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="tn-title" href="">Nulla vitae pharetra ligula</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="tn-img">
+                            <img src="img/top-news-4.jpg" />
+                            <div class="tn-content">
+                                <div class="tn-content-inner">
+                                    <a class="tn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="tn-title" href="">Ut ac euismod tellus a blandit</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="tn-img">
+                            <img src="img/top-news-5.jpg" />
+                            <div class="tn-content">
+                                <div class="tn-content-inner">
+                                    <a class="tn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="tn-title" href="">Cras ac egestas sem nec euismod</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Top News End-->
+
+
+<!-- Category News Start-->
+<div class="cat-news">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <h2><i class="fas fa-align-justify"></i>Sports</h2>
+                <div class="row cn-slider">
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-1.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Cras sed semper puru vitae lobortis velit</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-2.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Vestibulum ante ipsum primis</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-3.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Sed quis convallis lacus</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2><i class="fas fa-align-justify"></i>Technology</h2>
+                <div class="row cn-slider">
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-4.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Vivamus vel felis nec magna</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-5.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Phasellus vitae fermentum est</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-6.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Aenean ut varius dui</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Category News End-->
+
+
+<!-- Category News Start-->
+<div class="cat-news">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <h2><i class="fas fa-align-justify"></i>Business</h2>
+                <div class="row cn-slider">
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-7.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Interdum et malesuada fames ac ante</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-8.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Mauris non ligula eget ante sagittis</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-9.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Integer non nunc nec nulla aliquet</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h2><i class="fas fa-align-justify"></i>Entertainment</h2>
+                <div class="row cn-slider">
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-10.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Ut laoreet justo non ornare</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-11.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Proin a nulla ut enim feugiat</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="cn-img">
+                            <img src="img/cat-news-12.jpg" />
+                            <div class="cn-content">
+                                <div class="cn-content-inner">
+                                    <a class="cn-date" href=""><i class="far fa-clock"></i>05-Feb-2020</a>
+                                    <a class="cn-title" href="">Sed mauris sem sollicitudin at neque</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Category News End-->
+
+
+<!-- Main News Start-->
+
+<!-- Main News End-->
+
+@endsection
