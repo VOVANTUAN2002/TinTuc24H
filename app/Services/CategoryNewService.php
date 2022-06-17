c22a66a4c3a95924b818ec5fd1555b146a63bd39
vovantuan2002 <votuant2@gmail.com>
2022-06-17 09:45:49 +0700

    add CategoryNewServiceInterface

---
+++ app/Services/CategoryNewService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+
+class CategoryNewService implements CategoryNewServiceInterface
+{
+    protected $categorieRepository;
+
+    public function __construct(CategorieInterface $categorieRepository)
+    {
+        $this->categorieRepository = $categorieRepository;
+    }
+
+    public function getAll($request)
+    {
+        return $this->categorieRepository->getAll($request);
+    }
+
+    public function findById($id)
+    {
+        $categorie = $this->categorieRepository->findById($id);
+        return $categorie;
+    }
+
+    public function create($request)
+    {
+        $categorie = $this->categorieRepository->create($request);
+        return $categorie;
+    }
+
+    public function update($request, $id)
+    {
+        $oldCustomer = $this->categorieRepository->findById($id);
+        $this->categorieRepository->update($request, $oldCustomer);
+        return $oldCustomer;
+    }
+
+    public function destroy($id)
+    {
+        $categorie = $this->categorieRepository->findById($id);
+        $this->categorieRepository->destroy($categorie);
+        return $categorie;
+    }
+}
