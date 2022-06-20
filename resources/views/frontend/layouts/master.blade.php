47eead354a64ef788ac7a5a573103331ad220f60
vovantuan2002 <votuant2@gmail.com>
2022-06-20 10:13:19 +0700

    viet sub

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -50,12 +50,12 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 copyright">
-                        <p>Copyright &copy; <a href="https://htmlcodex.com">HTML Codex</a>. All Rights Reserved</p>
+                        <p>Bản quyền &copy; <a href="https://htmlcodex.com">HTML Codex</a>. Đã đăng ký Bản quyền</p>
                     </div>
 
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                     <div class="col-md-6 template-by">
-                        <p>Template By <a href="https://htmlcodex.com">HTML Codex</a></p>
+                        <p>Mẫu của <a href="https://htmlcodex.com">HTML Codex</a></p>
                     </div>
                 </div>
             </div>
